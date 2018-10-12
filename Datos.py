68ec542155f3cd3723b0dfd4179e01e47f1ee016
Martin <martinbonaro@hotmail.com>
2018-10-11 21:05:04 -0300

    Version a presentar

--- Datos.py
+++ Datos.py
@@ -6,20 +6,23 @@ Base = declarative_base()
 
 class Turno(Base):
     __tablename__ = 'turnos'
-    fecha = Column(DATE, primary_key=True, nullable=False)
-    hora = Column(TIME, primary_key=True, nullable=False)
-    usuario = Column(String, primary_key=False, nullable=False)
+    id = Column(INTEGER, primary_key=True, autoincrement=True, nullable=False)
+    fecha = Column(DATE, nullable=False)
+    hora = Column(TIME, nullable=False)
+    usuario = Column(String, nullable=False)
 
 class Usuario(Base):
     __tablename__ = 'usuarios'
-    nombre = Column(String, primary_key=True, nullable=False)
-    usuario = Column(String, primary_key=True, nullable=False)
-    contraseña =Column(String, primary_key=True, nullable=False)
+    id = Column(INTEGER, primary_key=True, autoincrement=True, nullable=False)
+    nombre = Column(String, nullable=False)
+    usuario = Column(String, nullable=False, unique=True)
+    contrasena =Column(String, primary_key=True, nullable=False)
 
 class Datos(object):
 
     def __init__(self):
-        engine = create_engine('mysql+pymysql://root:root@localhost/soportetpfinal')
+        engine = create_engine('mysql+mysqlconnector://sql10260351:TE3JvAT2Lr@sql10.freemysqlhosting.net/sql10260351')
+        #engine = create_engine('mysql+mysqlconnector://root:root@localhost/asd')
         Base.metadata.bind = engine
         db_session = sessionmaker()
         db_session.bind = engine
@@ -30,8 +33,13 @@ class Datos(object):
         self.session.commit()
         return entrada
 
-    def baja(self,fecha, hora):
-        self.session.query(Turno).filter(Turno.fecha == fecha and Turno.hora == hora).delete()
+    def bajaTurno(self, entrada):
+        self.session.query(Turno).filter(Turno.id == entrada.id).delete()
+        self.session.commit()
+        return True
+
+    def bajaUsuario(self, entrada):
+        self.session.query(Usuario).filter(Usuario.id == entrada.id).delete()
         self.session.commit()
         return True
 
@@ -46,11 +54,35 @@ class Datos(object):
         us = self.session.query(Usuario).filter(Usuario.usuario == usuario).all()
         if (len(us)==0):
             return 0
-        elif(us[0].contraseña == password):
+        elif(us[0].contrasena == password):
             return us
         else:
             return 0
 
-datos = Datos()
-datos.buscarTurnosFecha('2018-10-18')
 
+
+def pruebas():
+    datos = Datos()
+
+    # Alta de usuario
+    usuario = datos.alta(Usuario(nombre='jose prueba', usuario='prueba123', contrasena='123456'))
+
+    # Alta de turno
+    turno = datos.alta(Turno(fecha='2018-12-12', hora='12:00', usuario=usuario.usuario ))
+    assert turno.id>0
+
+    # Verificacion Usuario
+    assert (datos.verificarUsuario(usuario.usuario, usuario.contrasena)!=0)
+
+    # Busqueda
+    horas = datos.buscarTurnosFecha(turno.fecha)
+    assert (horas[0]==datetime.time.strftime(turno.hora, '%H'))
+    #assert (turno.hora == datetime.datetime.strptime(horas[0], '%H'))
+
+    # Bajas
+    datos.bajaTurno(turno)
+    datos.bajaUsuario(usuario)
+    assert (datos.verificarUsuario(usuario.usuario, usuario.contrasena) == 0)
+
+
+pruebas()
--- main.py
+++ main.py
@@ -17,64 +17,9 @@ from kivy.base import runTouchApp
 import calendar
 import time
 import datetime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import *
-from sqlalchemy.orm import sessionmaker
+from Datos import Turno, Usuario, Datos
 
 global USER
-
-Base = declarative_base()
-
-class Turno(Base):
-    __tablename__ = 'turnos'
-    fecha = Column(DATE, primary_key=True, nullable=False)
-    hora = Column(TIME, primary_key=True, nullable=False)
-    usuario = Column(String, primary_key=False, nullable=False)
-
-class Usuario(Base):
-    __tablename__ = 'usuarios'
-    nombre = Column(String, primary_key=True, nullable=False)
-    usuario = Column(String, primary_key=True, nullable=False)
-    contrasena =Column(String, primary_key=True, nullable=False)
-
-class Datos(object):
-
-    def __init__(self):
-        engine = create_engine('mysql+mysqlconnector://sql10260351:TE3JvAT2Lr@sql10.freemysqlhosting.net/sql10260351')
-        Base.metadata.bind = engine
-        db_session = sessionmaker()
-        db_session.bind = engine
-        self.session = db_session()
-
-    def alta(self, entrada):
-        self.session.add(entrada)
-        self.session.commit()
-        return entrada
-
-    def baja(self,fecha, hora):
-        self.session.query(Turno).filter(Turno.fecha == fecha and Turno.hora == hora).delete()
-        self.session.commit()
-        return True
-
-    def buscarTurnosFecha(self, fecha):
-        fech = self.session.query(Turno.hora).filter(Turno.fecha == fecha).all()
-        h=[]
-        for i in fech:
-            h.append(datetime.time.strftime(i.hora, '%H'))
-        return h
-
-    def verificarUsuario(self, usuario,password):
-        us = self.session.query(Usuario).filter(Usuario.usuario == usuario).all()
-        if (len(us)==0):
-            return 0
-        elif(us[0].contrasena == password):
-            return us
-        else:
-            return 0
-
-
-
-
 
 class Error(Popup):
     error = BoxLayout(orientation="vertical")
